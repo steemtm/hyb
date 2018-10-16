71b66967c9877c270cfdf78f93fe124abb0924a9
steemtm <38287977+steemtm@users.noreply.github.com>
2018-10-16 02:30:56 -0300

    Update hybridbotpromo.js

--- hybridbotpromo.js
+++ hybridbotpromo.js
@@ -17,9 +17,10 @@ const bot = new SteemBot({username, postingKey});
 bot.onDeposit(targetUsers, handleDeposit);
 
 function handleDeposit(data, responder) {
-  Responder.sendSbd(0.001, @hybridbot service is now live, send at least 0.010 SBD or STEEM to @hybridbot with the post you want upvoted as the memo. 0.050 SBD or STEEM max bid. Delegate 100 SP to unlock automatic upvotes and get daily payouts!)
   console.log('recevied %s deposit from %s to %s!', data.amount, data.from, data.to);
   console.log(data.memo);
+
+  Responder.sendSteem(0.001, '@hybridbot service is now live, send at least 0.010 SBD or STEEM to @hybridbot with the post you want upvoted as the memo. 0.050 SBD or STEEM max bid. Delegate 100 SP to unlock automatic upvotes and get daily payouts!');  
   // you get the money now give your service to user
 }
 
